f757e68b7ed37898799df3638aa239c01e1bcf81
Longarithm <the.aleksandr.logunov@gmail.com>
2021-08-10 23:33:52 +0300

    add config store

---
+++ runtime/runtime/src/config_store.rs
@@ -0,0 +1,20 @@
+use std::collections::BTreeMap;
+use near_primitives::version::ProtocolVersion;
+use crate::config::RuntimeConfig;
+use near_primitives::types::Gas;
+
+pub struct RuntimeConfigStore {
+    /// The runtime configuration taken from the genesis file but with possibly
+    /// modified `max_gas_burnt_view` limit.
+    store: BTreeMap<ProtocolVersion, RuntimeConfig>,
+}
+
+impl RuntimeConfigStore {
+    /// Constructs a new object from specified genesis runtime config.
+    ///
+    /// If `max_gas_burnt_view` is provided, the property in wasm limit
+    /// configuration will be adjusted to given value.
+    pub fn new(max_gas_burnt_view: Option<Gas>) -> Self {
+
+    }
+}
