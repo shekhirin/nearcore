b4f043eb866d06b06ec8f8dd22c13d511e3ba357
Longarithm <the.aleksandr.logunov@gmail.com>
2021-08-17 14:13:09 +0300

    fix test

--- core/primitives/src/runtime/config_store.rs
+++ core/primitives/src/runtime/config_store.rs
@@ -74,9 +74,9 @@ mod tests {
         let default_config = RuntimeConfig::default();
         let actual_runtime_config = ActualRuntimeConfig::new(default_config, None);
         let store = RuntimeConfigStore::new(None);
-        for protocol_version in [29u32, 34u32, 42u32, 50u32].cloned().iter() {
-            let old_config = actual_runtime_config.for_protocol_version(protocol_version);
-            let new_config = store.get_config(protocol_version);
+        for protocol_version in [29u32, 34u32, 42u32, 50u32].iter() {
+            let old_config = actual_runtime_config.for_protocol_version(protocol_version.clone());
+            let new_config = store.get_config(protocol_version.clone());
             assert_eq!(old_config, new_config);
         }
     }
